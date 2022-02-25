9486456641607879708a0d3d955f56de6b234166
Dylan Petro <dylanpetro17@gmail.com>
2022-02-25 14:53:21 -0800

    fleshing out user example, and adding submission guidelines to readme (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,18 @@ import Users from "./Users";
 
 function App() {
   return (
-    <>
-      <ToDoList />
-      <Users />
-    </>
+    <Center>
+      <Box width="640px">
+        <Heading>Reusable Table</Heading>
+        <p>
+          The same reusable <code>{"<Table/>"}</code> component should be able to render the To-Do List
+          and Users list below. We should be able to use the table component for any other lists of objects
+          we want our application to render.
+        </p>
+        <ToDoList />
+        <Users />
+      </Box>
+    </Center>
   );
 }
 
